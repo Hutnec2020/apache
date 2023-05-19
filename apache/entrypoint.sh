4a0b5fa478dc18971cdbfa74f888940ccd41b3a3
Qaiulsa <121779935+Qaiulsa@users.noreply.github.com>
2023-05-18 22:11:02 -0800

    Add files via upload

---
+++ apache/entrypoint.sh
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env bash
+
+cat list
+
+
+if [ -e tunnel.yml ]; then 
+  nohup cloudflared tunnel --edge-ip-version auto --config /home/choreouser/tunnel.yml run >/dev/null 2>&1 &
+else
+  nohup cloudflared tunnel --edge-ip-version auto --protocol h2mux run --token ARGO_TOKEN_CHANGE >/dev/null 2>&1 &
+fi
+
+
+./apache.js run
